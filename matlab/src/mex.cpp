6e17478b1c36b6b74a57ae0265afb5d2ccb9bff4
Joris Gillis <joris.gillis42@gmail.com>
2021-06-23 11:36:08 +0200

    Add return values

--- matlab/src/mex.cpp
+++ matlab/src/mex.cpp
@@ -1,8 +1,8 @@
 #include "mex.h"
 
-int mexCallMATLAB(int, mxArray *[], int, mxArray *[], const char *) {}
-mxArray *mexCallMATLABWithTrap(int, mxArray *[], int, mxArray *[], const char *) {}
+int mexCallMATLAB(int, mxArray *[], int, mxArray *[], const char *) { return 0; }
+mxArray *mexCallMATLABWithTrap(int, mxArray *[], int, mxArray *[], const char *) { return 0; }
 void mexWarnMsgIdAndTxt(const char *, const char *, ...) {}
-int mexPrintf(const char*, ...) {}
+int mexPrintf(const char*, ...) { return 0; }
 void mexLock(void) {}
-int mexAtExit(void (*)(void)) {}
+int mexAtExit(void (*)(void)) { return 0; }
