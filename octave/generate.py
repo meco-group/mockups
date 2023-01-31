a87a1e61e071eb87ee521c2a81a83a36f75cc026
Joris Gillis <joris.gillis42@gmail.com>
2023-01-31 23:26:17 +0100

    v30

--- octave/generate.py
+++ octave/generate.py
@@ -31,7 +31,7 @@ with open("include/mex.h","w") as fout:
   fout.write("#ifdef _WIN32\n")
   for decl in method_declarations:
     fout.write("#define %s adaptor_%s\n" % (get_name(decl),get_name(decl)))
-    fout.write( decl.replace(get_name(decl),"(*%s)" % get_name(decl))+"\n")
+    fout.write("__declspec( dllimport ) " + decl.replace(get_name(decl),"(*%s)" % get_name(decl))+"\n")
   fout.write("#else\n")
   for decl in method_declarations:
     fout.write(decl+"\n")
--- octave/include/mex.h
+++ octave/include/mex.h
@@ -56,117 +56,117 @@ typedef char mxChar;
 
 #ifdef _WIN32
 #define mxAddField adaptor_mxAddField
-int (*mxAddField)(mxArray *, const char *);
+__declspec( dllimport ) int (*mxAddField)(mxArray *, const char *);
 #define mxArrayToString adaptor_mxArrayToString
-char* (*mxArrayToString)(const mxArray *);
+__declspec( dllimport ) char* (*mxArrayToString)(const mxArray *);
 #define mxCreateCellMatrix adaptor_mxCreateCellMatrix
-mxArray* (*mxCreateCellMatrix)(mwSize, mwSize);
+__declspec( dllimport ) mxArray* (*mxCreateCellMatrix)(mwSize, mwSize);
 #define mxCreateCharMatrixFromStrings adaptor_mxCreateCharMatrixFromStrings
-mxArray* (*mxCreateCharMatrixFromStrings)(mwSize, const char **);
+__declspec( dllimport ) mxArray* (*mxCreateCharMatrixFromStrings)(mwSize, const char **);
 #define mxCreateDoubleMatrix adaptor_mxCreateDoubleMatrix
-mxArray* (*mxCreateDoubleMatrix)(mwSize, mwSize, mxComplexity);
+__declspec( dllimport ) mxArray* (*mxCreateDoubleMatrix)(mwSize, mwSize, mxComplexity);
 #define mxCreateDoubleScalar adaptor_mxCreateDoubleScalar
-mxArray* (*mxCreateDoubleScalar)(double);
+__declspec( dllimport ) mxArray* (*mxCreateDoubleScalar)(double);
 #define mxCreateLogicalMatrix adaptor_mxCreateLogicalMatrix
-mxArray* (*mxCreateLogicalMatrix)(mwSize, mwSize);
+__declspec( dllimport ) mxArray* (*mxCreateLogicalMatrix)(mwSize, mwSize);
 #define mxCreateLogicalScalar adaptor_mxCreateLogicalScalar
-mxArray* (*mxCreateLogicalScalar)(bool);
+__declspec( dllimport ) mxArray* (*mxCreateLogicalScalar)(bool);
 #define mxCreateNumericArray adaptor_mxCreateNumericArray
-mxArray* (*mxCreateNumericArray)(mwSize, const mwSize *, mxClassID, mxComplexity);
+__declspec( dllimport ) mxArray* (*mxCreateNumericArray)(mwSize, const mwSize *, mxClassID, mxComplexity);
 #define mxCreateNumericMatrix adaptor_mxCreateNumericMatrix
-mxArray* (*mxCreateNumericMatrix)(mwSize, mwSize, mxClassID, mxComplexity);
+__declspec( dllimport ) mxArray* (*mxCreateNumericMatrix)(mwSize, mwSize, mxClassID, mxComplexity);
 #define mxCreateSparse adaptor_mxCreateSparse
-mxArray* (*mxCreateSparse)(mwSize, mwSize, mwSize, mxComplexity);
+__declspec( dllimport ) mxArray* (*mxCreateSparse)(mwSize, mwSize, mwSize, mxComplexity);
 #define mxCreateString adaptor_mxCreateString
-mxArray* (*mxCreateString)(const char *);
+__declspec( dllimport ) mxArray* (*mxCreateString)(const char *);
 #define mxCreateStructMatrix adaptor_mxCreateStructMatrix
-mxArray* (*mxCreateStructMatrix)(mwSize, mwSize, int, const char **);
+__declspec( dllimport ) mxArray* (*mxCreateStructMatrix)(mwSize, mwSize, int, const char **);
 #define mxDestroyArray adaptor_mxDestroyArray
-void (*mxDestroyArray)(mxArray *);
+__declspec( dllimport ) void (*mxDestroyArray)(mxArray *);
 #define mxGetCell adaptor_mxGetCell
-mxArray* (*mxGetCell)(const mxArray *, mwIndex);
+__declspec( dllimport ) mxArray* (*mxGetCell)(const mxArray *, mwIndex);
 #define mxGetChars adaptor_mxGetChars
-mxChar* (*mxGetChars)(const mxArray *);
+__declspec( dllimport ) mxChar* (*mxGetChars)(const mxArray *);
 #define mxGetClassID adaptor_mxGetClassID
-mxClassID (*mxGetClassID)(const mxArray *);
+__declspec( dllimport ) mxClassID (*mxGetClassID)(const mxArray *);
 #define mxGetData adaptor_mxGetData
-void* (*mxGetData)(const mxArray *);
+__declspec( dllimport ) void* (*mxGetData)(const mxArray *);
 #define mxGetDimensions adaptor_mxGetDimensions
-const mwSize* (*mxGetDimensions)(const mxArray *);
+__declspec( dllimport ) const mwSize* (*mxGetDimensions)(const mxArray *);
 #define mxGetField adaptor_mxGetField
-mxArray* (*mxGetField)(const mxArray *, mwIndex, const char *);
+__declspec( dllimport ) mxArray* (*mxGetField)(const mxArray *, mwIndex, const char *);
 #define mxGetFieldByNumber adaptor_mxGetFieldByNumber
-mxArray* (*mxGetFieldByNumber)(const mxArray *, mwIndex, int);
+__declspec( dllimport ) mxArray* (*mxGetFieldByNumber)(const mxArray *, mwIndex, int);
 #define mxGetFieldNameByNumber adaptor_mxGetFieldNameByNumber
-const char* (*mxGetFieldNameByNumber)(const mxArray *, int);
+__declspec( dllimport ) const char* (*mxGetFieldNameByNumber)(const mxArray *, int);
 #define mxGetIr adaptor_mxGetIr
-mwIndex* (*mxGetIr)(const mxArray *);
+__declspec( dllimport ) mwIndex* (*mxGetIr)(const mxArray *);
 #define mxGetJc adaptor_mxGetJc
-mwIndex* (*mxGetJc)(const mxArray *);
+__declspec( dllimport ) mwIndex* (*mxGetJc)(const mxArray *);
 #define mxGetN adaptor_mxGetN
-size_t (*mxGetN)(const mxArray *);
+__declspec( dllimport ) size_t (*mxGetN)(const mxArray *);
 #define mxGetM adaptor_mxGetM
-size_t (*mxGetM)(const mxArray *);
+__declspec( dllimport ) size_t (*mxGetM)(const mxArray *);
 #define mxGetNumberOfDimensions adaptor_mxGetNumberOfDimensions
-mwSize (*mxGetNumberOfDimensions)(const mxArray *);
+__declspec( dllimport ) mwSize (*mxGetNumberOfDimensions)(const mxArray *);
 #define mxGetNumberOfElements adaptor_mxGetNumberOfElements
-mwSize (*mxGetNumberOfElements)(const mxArray *);
+__declspec( dllimport ) mwSize (*mxGetNumberOfElements)(const mxArray *);
 #define mxGetNumberOfFields adaptor_mxGetNumberOfFields
-int (*mxGetNumberOfFields)(const mxArray *);
+__declspec( dllimport ) int (*mxGetNumberOfFields)(const mxArray *);
 #define mxGetProperty adaptor_mxGetProperty
-mxArray* (*mxGetProperty)(const mxArray *, const mwIndex, const char *);
+__declspec( dllimport ) mxArray* (*mxGetProperty)(const mxArray *, const mwIndex, const char *);
 #define mxGetScalar adaptor_mxGetScalar
-double (*mxGetScalar)(const mxArray *);
+__declspec( dllimport ) double (*mxGetScalar)(const mxArray *);
 #define mxGetString adaptor_mxGetString
-int (*mxGetString)(const mxArray *, char *, mwSize);
+__declspec( dllimport ) int (*mxGetString)(const mxArray *, char *, mwSize);
 #define mxIsCell adaptor_mxIsCell
-bool (*mxIsCell)(const mxArray *);
+__declspec( dllimport ) bool (*mxIsCell)(const mxArray *);
 #define mxIsChar adaptor_mxIsChar
-bool (*mxIsChar)(const mxArray *);
+__declspec( dllimport ) bool (*mxIsChar)(const mxArray *);
 #define mxIsComplex adaptor_mxIsComplex
-bool (*mxIsComplex)(const mxArray *);
+__declspec( dllimport ) bool (*mxIsComplex)(const mxArray *);
 #define mxIsDouble adaptor_mxIsDouble
-bool (*mxIsDouble)(const mxArray *);
+__declspec( dllimport ) bool (*mxIsDouble)(const mxArray *);
 #define mxIsLogical adaptor_mxIsLogical
-bool (*mxIsLogical)(const mxArray *);
+__declspec( dllimport ) bool (*mxIsLogical)(const mxArray *);
 #define mxIsLogicalScalar adaptor_mxIsLogicalScalar
-bool (*mxIsLogicalScalar)(const mxArray *);
+__declspec( dllimport ) bool (*mxIsLogicalScalar)(const mxArray *);
 #define mxIsLogicalScalarTrue adaptor_mxIsLogicalScalarTrue
-bool (*mxIsLogicalScalarTrue)(const mxArray *);
+__declspec( dllimport ) bool (*mxIsLogicalScalarTrue)(const mxArray *);
 #define mxIsNumeric adaptor_mxIsNumeric
-bool (*mxIsNumeric)(const mxArray *);
+__declspec( dllimport ) bool (*mxIsNumeric)(const mxArray *);
 #define mxIsSparse adaptor_mxIsSparse
-bool (*mxIsSparse)(const mxArray *);
+__declspec( dllimport ) bool (*mxIsSparse)(const mxArray *);
 #define mxIsStruct adaptor_mxIsStruct
-bool (*mxIsStruct)(const mxArray *);
+__declspec( dllimport ) bool (*mxIsStruct)(const mxArray *);
 #define mxSetCell adaptor_mxSetCell
-void (*mxSetCell)(mxArray *, mwIndex, mxArray *);
+__declspec( dllimport ) void (*mxSetCell)(mxArray *, mwIndex, mxArray *);
 #define mxSetField adaptor_mxSetField
-void (*mxSetField)(mxArray *, mwIndex, const char *, mxArray *);
+__declspec( dllimport ) void (*mxSetField)(mxArray *, mwIndex, const char *, mxArray *);
 #define mxSetProperty adaptor_mxSetProperty
-void (*mxSetProperty)(mxArray *, mwIndex, const char *, const mxArray *);
+__declspec( dllimport ) void (*mxSetProperty)(mxArray *, mwIndex, const char *, const mxArray *);
 #define mxSetFieldByNumber adaptor_mxSetFieldByNumber
-void (*mxSetFieldByNumber)(mxArray *, mwIndex, int, mxArray *);
+__declspec( dllimport ) void (*mxSetFieldByNumber)(mxArray *, mwIndex, int, mxArray *);
 #define mxCreateCharArray adaptor_mxCreateCharArray
-mxArray* (*mxCreateCharArray)(mwSize, const mwSize *);
+__declspec( dllimport ) mxArray* (*mxCreateCharArray)(mwSize, const mwSize *);
 #define mexCallMATLAB adaptor_mexCallMATLAB
-int (*mexCallMATLAB)(int, mxArray *[], int, mxArray *[], const char *);
+__declspec( dllimport ) int (*mexCallMATLAB)(int, mxArray *[], int, mxArray *[], const char *);
 #define mexCallMATLABWithTrap adaptor_mexCallMATLABWithTrap
-mxArray* (*mexCallMATLABWithTrap)(int, mxArray *[], int, mxArray *[], const char *);
+__declspec( dllimport ) mxArray* (*mexCallMATLABWithTrap)(int, mxArray *[], int, mxArray *[], const char *);
 #define mexErrMsgIdAndTxt adaptor_mexErrMsgIdAndTxt
-void (*mexErrMsgIdAndTxt)(const char *, const char *, ...);
+__declspec( dllimport ) void (*mexErrMsgIdAndTxt)(const char *, const char *, ...);
 #define mexErrMsgTxt adaptor_mexErrMsgTxt
-void (*mexErrMsgTxt)(const char *);
+__declspec( dllimport ) void (*mexErrMsgTxt)(const char *);
 #define mexEvalString adaptor_mexEvalString
-int (*mexEvalString)(const char *);
+__declspec( dllimport ) int (*mexEvalString)(const char *);
 #define mexWarnMsgIdAndTxt adaptor_mexWarnMsgIdAndTxt
-void (*mexWarnMsgIdAndTxt)(const char *, const char *, ...);
+__declspec( dllimport ) void (*mexWarnMsgIdAndTxt)(const char *, const char *, ...);
 #define mexPrintf adaptor_mexPrintf
-int (*mexPrintf)(const char*, ...);
+__declspec( dllimport ) int (*mexPrintf)(const char*, ...);
 #define mexLock adaptor_mexLock
-void (*mexLock)(void);
+__declspec( dllimport ) void (*mexLock)(void);
 #define mexAtExit adaptor_mexAtExit
-int (*mexAtExit)(void (*)(void));
+__declspec( dllimport ) int (*mexAtExit)(void (*)(void));
 #else
 int mxAddField(mxArray *, const char *);
 char* mxArrayToString(const mxArray *);
