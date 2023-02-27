b09b35c3a94f91dab8605c1279a7d4b5e389d29c
Joris Gillis <joris.gillis42@gmail.com>
2023-02-27 12:01:29 +0100

    Constants

--- gurobi/include/gurobi_c.h
+++ gurobi/include/gurobi_c.h
@@ -39,6 +39,61 @@ typedef struct _GRBenv GRBenv;
     #endif
 #endif
 
+#define GRB_CONTINUOUS 'C'
+#define GRB_BINARY     'B'
+#define GRB_INTEGER    'I'
+#define GRB_SEMICONT   'S'
+#define GRB_SEMIINT    'N'
+
+#define GRB_LOADED          1
+#define GRB_OPTIMAL         2
+#define GRB_INFEASIBLE      3
+#define GRB_INF_OR_UNBD     4
+#define GRB_UNBOUNDED       5
+#define GRB_CUTOFF          6
+#define GRB_ITERATION_LIMIT 7
+#define GRB_NODE_LIMIT      8
+#define GRB_TIME_LIMIT      9
+#define GRB_SOLUTION_LIMIT 10
+#define GRB_INTERRUPTED    11
+#define GRB_NUMERIC        12
+#define GRB_SUBOPTIMAL     13
+#define GRB_INPROGRESS     14
+#define GRB_USER_OBJ_LIMIT 15
+
+#define GRB_DBL_ATTR_X         "X"
+#define GRB_DBL_ATTR_XN        "Xn"
+#define GRB_DBL_ATTR_BARX      "BarX"
+#define GRB_DBL_ATTR_RC        "RC"
+#define GRB_DBL_ATTR_VDUALNORM "VDualNorm"
+#define GRB_INT_ATTR_VBASIS    "VBasis"
+
+#define GRB_DBL_ATTR_PI        "Pi"
+#define GRB_DBL_ATTR_QCPI      "QCPi"
+#define GRB_DBL_ATTR_SLACK     "Slack"
+#define GRB_DBL_ATTR_QCSLACK   "QCSlack"
+#define GRB_DBL_ATTR_CDUALNORM "CDualNorm"
+#define GRB_INT_ATTR_CBASIS    "CBasis"
+
+#define GRB_LESS_EQUAL    '<'
+#define GRB_GREATER_EQUAL '>'
+#define GRB_EQUAL         '='
+
+#define GRB_MINIMIZE 1
+#define GRB_MAXIMIZE -1
+
+#define GRB_SOS_TYPE1 1
+#define GRB_SOS_TYPE2 2
+
+#define GRB_INFINITY  1e100
+#define GRB_UNDEFINED 1e101
+#define GRB_MAXINT    2000000000
+
+#define GRB_MAX_NAMELEN    255
+#define GRB_MAX_STRLEN     512
+#define GRB_MAX_TAGLEN    10240
+#define GRB_MAX_CONCURRENT 64
+
 #define GRBaddconstr adaptor_GRBaddconstr
 DLLSYMBOL int (__stdcall *GRBaddconstr)(GRBmodel *, int, int *, double *, char, double, const char *);
 #define GRBaddqconstr adaptor_GRBaddqconstr
