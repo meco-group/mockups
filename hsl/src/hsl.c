816e334a864938d06156fe16661e7dfce40ae1b1
Joris Gillis <joris.gillis42@gmail.com>
2023-03-01 19:39:37 +0100

    Adding hsl

---
+++ hsl/src/hsl.c
@@ -0,0 +1,14 @@
+void ma27id_(int* ICNTL, double* CNTL) {}
+void ma27ad_(int *N, int *NZ, const int *IRN, const int* ICN,
+           int *IW, int* LIW, int* IKEEP, int *IW1,
+           int* NSTEPS, int* IFLAG, int* ICNTL,
+           double* CNTL, int *INFO, double* OPS) {}
+void ma27bd_(int *N, int *NZ, const int *IRN, const int* ICN,
+           double* A, int* LA, int* IW, int* LIW,
+           int* IKEEP, int* NSTEPS, int* MAXFRT,
+           int* IW1, int* ICNTL, double* CNTL,
+           int* INFO) {}
+void ma27cd_(int *N, double* A, int* LA, int* IW,
+           int* LIW, double* W, int* MAXFRT,
+           double* RHS, int* IW1, int* NSTEPS,
+           int* ICNTL, double* CNTL) {}
