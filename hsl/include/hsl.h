fb26a096683fd01bcba6153ac981b8567cf2c4f8
Joris Gillis <joris.gillis42@gmail.com>
2023-08-04 21:45:29 +0200

    bump v64

--- hsl/include/hsl.h
+++ hsl/include/hsl.h
@@ -13,4 +13,10 @@ extern "C" {
                int* LIW, double* W, int* MAXFRT,
                double* RHS, int* IW1, int* NSTEPS,
                int* ICNTL, double* CNTL);
+  
+void ma57id_(double cntl[], int icntl[]);
+void ma57ad_(const int* n, const int* ne, const int irn[], const int jcn[], const int* lkeep, int keep[], int iwork[], int icntl[], int info[], double rinfo[]);
+void ma57bd_(const int* n, int* ne, const double a[], double fact[], const int* lfact, int ifact[], const int* lifact, const int* lkeep, const int keep[], int iwork[], int icntl[], double cntl[], int info[], double rinfo[]);
+void ma57cd_(const int* job, const int* n, double fact[], int* lfact, int ifact[], int* lifact, const int* nrhs, double rhs[], const int* lrhs, double work[], int* lwork, int iwork[], int icntl[], int info[]);
+void ma57dd_(const int* job, const int* n, int* ne, const double a[], const int irn[], const int jcn[], double fact[], int* lfact, int ifact[], int* lifact, const double rhs[], double x[], double resid[], double work[], int iwork[], int icntl[], double cntl[], int info[], double rinfo[]);
 }
